2fb390ff2a6b18929979103ce71f617c98aae117
walter castro <walterc.100@gmail.com>
2024-06-20 15:52:22 -0500

    adicione la funcion sumar

--- funciones.js
+++ funciones.js
@@ -4,3 +4,11 @@ function saludar(){
     console.log('Hola como estan muchachos')
 }
 saludar()
+
+function sumar(a,b){
+
+    return a+b
+}
+
+const suma= sumar(5,8)
+console.log('La suama es '+ suma)
