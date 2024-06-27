0f5dcf88ac9b1e23d1e920a30e3dd75beac72ebc
walter castro <walterc.100@gmail.com>
2024-06-27 13:39:48 -0500

    probando con la rama r-400

--- Tarea_2/funcion.js
+++ Tarea_2/funcion.js
@@ -1,5 +1,18 @@
+// function saludar(){
+//     console.log('Hola como estan todos')
+// }
+// saludar()
 
-function saludar(){
-    console.log('Hola como estan todos')
-}
-saludar()
+//! primera clase de javaScript
+let numero1 = prompt("digita en primer numero");
+let numero2 = prompt("digita en primer numero");
+
+console.log(numero1);
+console.log(numero2);
+
+console.log(numero1 === numero2);
+console.log(numero1 !== numero2);
+console.log(numero1 > numero2);
+console.log(numero1 > numero2);
+console.log(numero1 >= numero2);
+console.log(numero1 <= numero2);
