b4cbfcbc6a34e398d93b3cb36467302231265d4e
walter castro <walterc.100@gmail.com>
2024-06-28 10:07:13 -0500

    cambios

---
+++ markdown/myReadme.md
@@ -0,0 +1,22 @@
+
+<!-- HEADERS -->
+# H1
+## H2
+### H3
+#### H4
+##### H5
+###### H6
+
+<!-- Texto en cursiva -->
+esto es un texto en *cursiva*
+
+<!-- Texto en negrita -->
+esto es un texto en **negrita**
+
+<!-- Texto tachado (Strikethrough) -->
+esto es un texto que está ~~tachado~~
+
+<!-- Texto combinado con negrita y cursiva -->
+combina negrita con **asteriscos y _underscores_**
+
+<!-- LISTAS Y SUBLISTAS-->
---
+++ tarea_3/tarea.js
@@ -0,0 +1,32 @@
+
+
+
+//*Crear un archivo .js y desarrollar un script que muestre en consola los numeros del 1 al 50.
+
+// for(let i=1; i<=50;i++){
+//     console.log('numero '+i)
+// }
+
+//* Modificar el script para que ahora imprima en consola los numeros del 1 al 2000.
+// for(let i=1; i<=2000;i++){
+//     console.log('numero '+i)
+// }
+
+//*Modificar el script para que ahora imprima solo los numeros que empiezan por 2 (convertir a string el numero y verificar con starsWith)
+
+
+// for(let i=1; i<=2000;i++){
+
+//     if(i.toString().startsWith('2')){
+//         console.log('numero '+i)
+//     }
+   
+// }
+
+for(let i=1; i<=50;i++){
+
+    if(i.toString()[0]==='2'){
+        console.log('numero '+[i])
+    }
+   
+}
